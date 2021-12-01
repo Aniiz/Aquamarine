2f318f1972f67b76c36c9584ba3a2d93147fa868
Aniz <Anizmoreira@hotmail.com>
2021-12-01 17:43:40 -0300

    addmenuinf

---
+++ js/script.js
@@ -0,0 +1,22 @@
+/* -----------------------------------------------------------------------------------------------------------
+        Script da vitriine */
+
+const imgs = document.getElementById("img");
+const img = document.querySelectorAll("#img img")
+
+let idx = 0;
+
+function carrossel(){
+    idx++;
+
+    if(idx > img.length - 1){
+        idx = 0;
+    }
+
+    imgs.style.transform = `translateX(${-idx*70}vw)`
+}
+
+setInterval(carrossel, 1800);
+
+/* -----------------------------------------------------------------------------------------------------------
+        Script dark mode */
